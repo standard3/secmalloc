9a50e22dca9dd255abfdf40e2672b23f2cf6eee8
theo.abel <theo.abel@ecole2600.com>
2024-05-03 15:08:58 +0200

    feat: restored initial state

--- include/my_secmalloc.private.h
+++ include/my_secmalloc.private.h
@@ -2,6 +2,7 @@
 #define _SECMALLOC_PRIVATE_H
 
 #include "my_secmalloc.h"
+#include "utils.h"
 
 typedef enum
 {
@@ -22,8 +23,7 @@ typedef struct
     size_t available;
 } chunk_list_t;
 
-void *
-my_malloc(size_t size);
+void *my_malloc(size_t size);
 void my_free(void *ptr);
 void *my_calloc(size_t nmemb, size_t size);
 void *my_realloc(void *ptr, size_t size);
