34cdcca6489683bdc2fdd656842975611a42ba94
theo.abel <theo.abel@ecole2600.com>
2024-05-29 21:34:43 +0200

    feat: added few tests ensuring stability

--- test/test.c
+++ test/test.c
@@ -8,7 +8,7 @@
 #include "my_secmalloc.private.h"
 
 extern const size_t heap_size;
-extern const size_t heap_offset_size;
+extern const size_t metadata_offset;
 
 Test(mmap, simple_mmap)
 {
@@ -32,7 +32,7 @@ Test(allocation, init_heap)
     cr_expect_null(heap->chunk);
     cr_expect_null(heap->next);
 
-    int res = munmap(heap, heap_size * heap_offset_size);
+    int res = munmap(heap, heap_size * metadata_offset);
     cr_expect(res == 0);
 }
 
@@ -67,3 +67,63 @@ Test(allocation, allocate_huge_chunk)
     int res = munmap(chunk, chunk_size);
     cr_expect(res == 0);
 }
+
+Test(allocation, allocate_multiple_chunks)
+{
+    chunk_t *chunk1 = allocate_chunk(3);
+    cr_expect_not_null(chunk1);
+    cr_expect_eq(chunk1->size, 3);
+
+    chunk_t *chunk2 = allocate_chunk(5);
+    cr_expect_not_null(chunk2);
+    cr_expect_eq(chunk2->size, 5);
+
+    chunk_t *chunk3 = allocate_chunk(8);
+    cr_expect_not_null(chunk3);
+    cr_expect_eq(chunk3->size, 8);
+
+    int res1 = munmap(chunk1, 3);
+    cr_expect(res1 == 0);
+
+    int res2 = munmap(chunk2, 5);
+    cr_expect(res2 == 0);
+
+    int res3 = munmap(chunk3, 8);
+    cr_expect(res3 == 0);
+}
+
+Test(allocation, allocate_and_free)
+{
+    init_logging();
+    chunk_t *chunk = allocate_chunk(3);
+    cr_expect_not_null(chunk);
+    cr_expect_eq(chunk->size, 3);
+
+    my_free(chunk);
+
+    cr_expect_eq(chunk->state, FREE);
+
+    close_logging();
+}
+
+// Test(allocation, allocate_multiple_chunks_and_free)
+// {
+//     init_logging();
+//     chunk_t *chunk1 = allocate_chunk(3);
+//     cr_expect_not_null(chunk1);
+//     cr_expect_eq(chunk1->size, 3);
+
+//     chunk_t *chunk2 = allocate_chunk(5);
+//     cr_expect_not_null(chunk2);
+//     cr_expect_eq(chunk2->size, 5);
+
+//     chunk_t *chunk3 = allocate_chunk(8);
+//     cr_expect_not_null(chunk3);
+//     cr_expect_eq(chunk3->size, 8);
+
+//     my_free(chunk1);
+//     my_free(chunk2);
+//     my_free(chunk3);
+
+//     close_logging();
+// }
